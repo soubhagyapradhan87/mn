24b29f36d13171538dfdf1e488452640f2d6e00f
soubhagyapradhan <soubhagyapradhan87@gmail.com>
2018-08-27 14:48:54 +0530

    initial developed programmes

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,47 @@
-import 'package:flutter/material.dart';
-import 'home.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:mrun1/model/posts.dart';
 
-void main() {
+import 'package:flutter/material.dart';
+import 'ui/splashScreen.dart';
+import 'package:mrun1/repo/repo.dart';
+import 'package:mrun1/ui/homeScreen.dart';
+
+void main() async{
 
   runApp(
       new MaterialApp(
-        title: "MrunApp123",
+        title: "MrunApp",
         debugShowCheckedModeBanner: false,
-        home: Home(),
+        home: new SplashScreen(),
       )
   );
+}
+
+
+Future<String> getJson() async {
+  String apiurl = "http://mrunalinee.com/wp-json/wp/v2/posts";
+  String imgurl="http://mrunalinee.com/wp-json/wp/v2/media?parent=";
+  http.Response response = await http.get(apiurl);
+  RepoData.data=JSON.decode(response.body);
+  //print(RepoData.data);
+  List<Post> list=new List<Post>();
+  for(var x in RepoData.data){
+    String a=x["id"].toString();
+    http.Response response1 = await http.get(imgurl+a);
+    //print(imgurl+a);
+    List imageData=JSON.decode(response1.body);
+    Post p=new Post(title: x["title"]["rendered"], id: a, link: x["link"], imglink: imageData[0]["guid"]["rendered"],date: x["date"]);
+    list.add(p);
+
+  }
+
+  Posts posts=await new Posts(name: "Mrunalinee", location: "Bangalore", logo: "assets/back.png",
+      backdropPhoto: "assets/thumbnail.png", about: "One Stop For Authentic Odia Recipes", post: list);
+  RepoData.posts=posts;
+  print(RepoData.posts);
+  return  "succss";
+
 
 }

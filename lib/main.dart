c5f8cbd6c7bbec1ad425622cf47b848ac7a26df0
sunil <selectsunil@gmail.com>
2018-09-06 11:37:31 +0530

    Test push2

--- lib/main.dart
+++ lib/main.dart
@@ -30,8 +30,8 @@ Future<String> getJson() async {
 
   http.Response responseCatUrl = await http.get(catUrl);
   RepoData.catJsonList=json.decode(responseCatUrl.body);
-   //print("catjson lenth ${RepoData.catJsonList.length}");
-  print("sunil");
+   print("catjson lenth ${RepoData.catJsonList.length}");
+  //print("sunil");
   List<Category> catList=new List<Category>();
 
 
